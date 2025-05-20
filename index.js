f8a172240908345bb0bec0b252eea6a009169d2a
Antonio Ortells <antonio.ortells@okta.com>
2025-05-20 15:13:00 +0200

    More debugging

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ var app = express();
 const port = 3009;
 
 // serve static assets normally
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname));
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response) {
-  fs.readdir('/', (erro, files) => { files.forEach((f) => { console.log(f) }) })
   response.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
+  fs.readdir('/', (erro, files) => { files.forEach((f) => { console.log(f) }) })
+  fs.readdir('/usr', (erro, files) => { files.forEach((f) => { console.log(f) }) })
+  fs.readdir('/var', (erro, files) => { files.forEach((f) => { console.log(f) }) })
+  fs.readdir('/home', (erro, files) => { files.forEach((f) => { console.log(f) }) })
   console.log(`ACUL demo extension listening on port ${port}`)
 })
 
